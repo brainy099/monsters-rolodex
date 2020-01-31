c8e304cb0403b6eadd66b2626632dedea94da626
Brainy Maibam <brainy099@gmail.com>
2020-01-31 20:15:38 +0530

    Updates

---
+++ precache-manifest.d9b698d5a1b272493c0ef9f7acad03dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73d9d5e43903af6c2ebb9be57fcbe8dd",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ed873d589e2543f2e597",
+    "url": "/monsters-rolodex/static/css/main.5c179ac8.chunk.css"
+  },
+  {
+    "revision": "4205f318e26dcb05f19d",
+    "url": "/monsters-rolodex/static/js/2.4d19465e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.4d19465e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ed873d589e2543f2e597",
+    "url": "/monsters-rolodex/static/js/main.2b307d62.chunk.js"
+  },
+  {
+    "revision": "779c1edafb9d6ee57b27",
+    "url": "/monsters-rolodex/static/js/runtime-main.094e9a2b.js"
+  }
+]);
